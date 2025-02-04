65cae0108241658f085ff3ec1fdedc625a5af6c8
Nathan Jaremko <nathan@jaremko.ca>
2025-02-03 23:28:44 -0800

    Fix CI

--- lib/parquet.rb
+++ lib/parquet.rb
@@ -1,5 +1,10 @@
 require_relative "parquet/version"
-require_relative "parquet/parquet"
+
+begin
+  require "parquet/#{RUBY_VERSION.to_f}/parquet"
+rescue LoadError
+  require "parquet/parquet"
+end
 
 module Parquet
 end
